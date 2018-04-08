810617958519c6a63937e190d791c149bb3d1cd9
Tim Simpson <timsimpson4@gmail.com>
2018-04-08 17:10:51 -0500

    Bump version: 0.1.1 → 0.1.2

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open(os.path.join(ROOT, 'README.rst')) as file:
 
 setuptools.setup(
     name='frontdoor',
-    version='0.1.1',
+    version='0.1.2',
     description='Aids the creation of "front door" scripts.',
     long_description=description,
     author='Tim Simpson',
