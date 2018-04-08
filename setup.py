ef7e18a4c5d39fc4637bddb168621059bdf5e40f
Tim Simpson <timsimpson4@gmail.com>
2018-04-08 16:58:00 -0500

    release frontdoor on PyPi

--- setup.py
+++ setup.py
@@ -1,5 +1,13 @@
 #!/usr/bin/env python
+import os
+
 import setuptools
+
+
+ROOT = os.path.join(os.path.dirname(os.path.realpath(__file__)))
+with open(os.path.join(ROOT, 'README.rst')) as file:
+    description = file.read()
+
 
 setuptools.setup(
     name='frontdoor',
